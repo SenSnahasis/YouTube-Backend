35c4611ca2d603be2b9ee2871a6c92dd44bc2cec
SenSnahasis <snahasis1001@gmail.com>
2025-06-15 02:48:34 +0530

    Add delete from cloudinary method

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,17 +1,26 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 
 // Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+const extractPublicId = (cloudinaryUrl) => {
+    // Example: https://res.cloudinary.com/demo/video/upload/v1654321234/folder/abc123.mp4
+    const parts = cloudinaryUrl.split("/");
+    const fileName = parts[parts.length - 1]; // abc123.mp4
+    const publicId = fileName.split(".")[0];  // abc123
+
+    return publicId;
+};
+
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
+        if (!localFilePath) return null
         // file upload on cloudinary
         const uploadResponse = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
@@ -28,4 +37,21 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadOnCloudinary}
+const deleteFromCloudinary = async (fileUrl, resourceType = "image") => {
+    try {
+        const publicId = extractPublicId(fileUrl)
+        const result = await cloudinary.uploader.destroy(publicId, {
+            resource_type: resourceType,
+        });
+        console.log(`Successfully deleted the ${resourceType}`);
+        return result;
+    } catch (error) {
+        console.error("Cloudinary delete error:", error);
+        return null;
+    }
+};
+
+export {
+    uploadOnCloudinary,
+    deleteFromCloudinary
+}
